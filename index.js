c7586e6c9d33632fc22e7d82cf65e58eb56d2ae6
Iulian Irimina <iirimina@schools.nyc.gov>
2024-11-11 11:36:50 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+/*
+Challenge:
+Make it so that when you click the 'Add to cart' button, whatever is written in the input field should be console logged.
+Write the .js code.
+*/
+
+Hints:
+//connect HTML with JS by setting up some constant variables 
+//attach an event listener to the add button
+//test the functionality 
+
